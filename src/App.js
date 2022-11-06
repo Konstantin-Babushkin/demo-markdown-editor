51d45843881123340b64aba1fb0820efe7bd4c0a
Konstantin-Babushkin <constant-babushkin@yandex.ru>
2022-11-06 14:23:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import { marked } from "marked";
 
 function App() {
+  const [inputText, setInput] = useState(`Heading
+=======
+
+Sub-heading
+-----------
+ 
+### Another deeper heading
+ 
+Paragraphs are separated
+by a blank line.
+
+![tommy wiseau](https://pyxis.nymag.com/v1/imgs/b25/753/ef0495a30eac613c74c5db75f6036ba7fc-14-the-room-tommy-wiseau.rsquare.w700.jpg)
+
+Leave 2 spaces at the end of a line to do a  
+line break
+
+Text attributes *italic*, **bold**, 
+\`monospace\`, ~~strikethrough~~.
+
+Shopping list:
+
+  * bananas
+  * carrots
+  * bokoya
+
+Numbered list:
+
+  1. pasta
+  2. sempa
+  3. julian
+
+> Hi Mark
+
+The rain---not the reign---snow dust.
+
+\`\`\`
+I'm a code block!
+\`\`\`
+
+ *[ffc](https://freecodecamp.com)*
+`);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const createMarkup = () => {
+    var options = { sanitize: true };
+    return { __html: marked(inputText, options) };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <textarea
+        id="editor"
+        autofocus
+        rows="5"
+        cols="50"
+        onChange={handleChange}
+      >
+        {inputText}
+      </textarea>
+
+      <div id="preview" dangerouslySetInnerHTML={createMarkup()}></div>
     </div>
   );
 }
